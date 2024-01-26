a0879755906214481d4a4942b90bc529e9290a8d
Christo du Toit <christo@dutoit.co.uk>
2024-01-26 23:37:24 +0000

    INFRA: Initial check-in

---
+++ STX.REST.RESTFulSense.Infrastructure.Build/Program.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using STX.REST.RESTFulSense.Infrastructure.Build.Services;
+
+namespace STX.REST.RESTFulSense.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
---
+++ STX.REST.RESTFulSense.Infrastructure.Build/Services/ScriptGenerationService.cs
@@ -0,0 +1,122 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV3s;
+
+namespace STX.REST.RESTFulSense.Infrastructure.Build.Services
+{
+    internal class ScriptGenerationService
+    {
+        private readonly ADotNetClient adotNetClient;
+
+        public ScriptGenerationService() =>
+            adotNetClient = new ADotNetClient();
+
+        public void GenerateBuildScript()
+        {
+            string branchName = "main";
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "Build",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { branchName }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Types = new string[] { "opened", "synchronize", "reopened", "closed" },
+                        Branches = new string[] { branchName }
+                    }
+                },
+
+                EnvironmentVariables = new Dictionary<string, string>
+                {
+                    { "IS_RELEASE_CANDIDATE", EnvironmentVariables.IsGitHubReleaseCandidate() }
+                },
+
+                Jobs = new Dictionary<string, Job>
+                {
+                    {
+                        "build",
+                        new Job
+                        {
+                            RunsOn = BuildMachines.UbuntuLatest,
+
+                            Steps = new List<GithubTask>
+                            {
+                                new CheckoutTaskV3
+                                {
+                                    Name = "Check out"
+                                },
+
+                                new SetupDotNetTaskV3
+                                {
+                                    Name = "Setup .Net",
+
+                                    With = new TargetDotNetVersionV3
+                                    {
+                                        DotNetVersion = "7.0.201"
+                                    }
+                                },
+
+                                new RestoreTask
+                                {
+                                    Name = "Restore"
+                                },
+
+                                new DotNetBuildTask
+                                {
+                                    Name = "Build"
+                                },
+
+                                new TestTask
+                                {
+                                    Name = "Test"
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "add_tag",
+                        new TagJob(
+                            runsOn: BuildMachines.UbuntuLatest,
+                            dependsOn: "build",
+                            projectRelativePath: "STX.REST.RESTFulSense/STX.REST.RESTFulSense.csproj",
+                            githubToken: "${{ secrets.PAT_FOR_TAGGING }}",
+                            branchName: branchName)
+                    },
+                    {
+                        "publish",
+                        new PublishJob(
+                            runsOn: BuildMachines.UbuntuLatest,
+                            dependsOn: "add_tag",
+                            nugetApiKey: "${{ secrets.NUGET_ACCESS }}")
+                    }
+                }
+            };
+
+            string buildScriptPath = "../../../../.github/workflows/build.yml";
+            string directoryPath = Path.GetDirectoryName(buildScriptPath);
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            adotNetClient.SerializeAndWriteToFile(
+                adoPipeline: githubPipeline,
+                path: buildScriptPath);
+        }
+    }
+}
