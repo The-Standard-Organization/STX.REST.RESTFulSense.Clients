f2fb62f2667b5c9ae10126440fe99199b5cdad98
Christo du Toit <christo@dutoit.co.uk>
2024-01-30 16:56:16 +0000

    CODE RUB: Hardcoded the status details for now.

--- STX.REST.RESTFulSense.Clients/Brokers/ErrorBrokers/ErrorBroker.cs
+++ STX.REST.RESTFulSense.Clients/Brokers/ErrorBrokers/ErrorBroker.cs
@@ -2,25 +2,9 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Newtonsoft.Json;
-using STX.REST.RESTFulSense.Clients.Models.Brokers.StatusDetails;
-
 namespace STX.REST.RESTFulSense.Clients.Brokers.ErrorBrokers
 {
     internal partial class ErrorBroker : IErrorBroker
     {
-        public ErrorBroker() =>
-            statusDetails = InitialiseStatusCodes();
-
-        private static IQueryable<StatusDetail> InitialiseStatusCodes()
-        {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "Data\\StatusCodes.json");
-            string json = File.ReadAllText(path);
-
-            return JsonConvert.DeserializeObject<List<StatusDetail>>(json).AsQueryable();
-        }
     }
 }
