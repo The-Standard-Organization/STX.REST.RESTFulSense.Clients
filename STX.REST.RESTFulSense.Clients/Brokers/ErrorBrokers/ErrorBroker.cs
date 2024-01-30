9fc10611c52ac2fe2219b45ecd0537bab4be422d
Christo du Toit <christo@dutoit.co.uk>
2024-01-30 17:25:21 +0000

    CODE RUB: Code clean-up

--- STX.REST.RESTFulSense.Clients/Brokers/ErrorBrokers/ErrorBroker.cs
+++ STX.REST.RESTFulSense.Clients/Brokers/ErrorBrokers/ErrorBroker.cs
@@ -2,9 +2,14 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System.Linq;
+using STX.REST.RESTFulSense.Clients.Models.Brokers.StatusDetails;
+
 namespace STX.REST.RESTFulSense.Clients.Brokers.ErrorBrokers
 {
     internal partial class ErrorBroker : IErrorBroker
     {
+        public IQueryable<StatusDetail> SelectAllStatusDetails() =>
+            statusDetails.AsQueryable();
     }
 }
