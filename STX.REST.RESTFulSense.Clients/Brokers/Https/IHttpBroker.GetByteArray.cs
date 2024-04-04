09ee3658763d745f5adad84b0677a17d15ddf67b
Greg Hays <glhays.dev@gmail.com>
2024-04-03 19:21:54 -0700

    BROKERS: HttpBroker `GetByteArrayAsync` Add
    Closes: #21
    Closes: #22

---
+++ STX.REST.RESTFulSense.Clients/Brokers/Https/IHttpBroker.GetByteArray.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace STX.REST.RESTFulSense.Clients.Brokers.Https
+{
+    internal partial interface IHttpBroker
+    {
+        ValueTask<HttpResponseMessage> GetByteArrayAsync(
+            string relativeUrl);
+
+        ValueTask<HttpResponseMessage> GetByteArrayAsync(
+            string relativeUrl,
+            CancellationToken cancellationToken);
+    }
+}
