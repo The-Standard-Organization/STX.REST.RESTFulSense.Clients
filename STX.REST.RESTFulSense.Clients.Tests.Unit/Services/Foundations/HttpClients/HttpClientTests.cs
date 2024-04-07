5877d088295737cd2e1031d1f181769f14cfb31b
Raúl LB <rlorenb@gmail.com>
2024-04-07 22:24:33 +0200

    ShouldGetAsync -> WIP

--- STX.REST.RESTFulSense.Clients.Tests.Unit/Services/Foundations/HttpClients/HttpClientTests.cs
+++ STX.REST.RESTFulSense.Clients.Tests.Unit/Services/Foundations/HttpClients/HttpClientTests.cs
@@ -2,14 +2,16 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using Force.DeepCloner;
+using Moq;
+using STX.REST.RESTFulSense.Clients.Brokers.Https;
+using STX.REST.RESTFulSense.Clients.Models.HttpClients;
+using STX.REST.RESTFulSense.Clients.Services.Foundations.HttpClients;
 using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using Moq;
-using STX.REST.RESTFulSense.Clients.Brokers.Https;
-using STX.REST.RESTFulSense.Clients.Services.Foundations.HttpClients;
 using Tynamix.ObjectFiller;
 
 namespace STX.REST.RESTFulSense.Clients.Tests.Unit.Services.Foundations.HttpClients
@@ -25,10 +27,10 @@ namespace STX.REST.RESTFulSense.Clients.Tests.Unit.Services.Foundations.HttpClie
             this.httpClientService =
                 new HttpClientService(httpClientBroker.Object);
         }
-        
+
         private static string GetRandomString() =>
             new MnemonicString().GetValue();
-        
+
         private static StreamContent CreateStreamContent(string content)
         {
             byte[] contentBytes = Encoding.ASCII.GetBytes(content);
@@ -36,14 +38,66 @@ namespace STX.REST.RESTFulSense.Clients.Tests.Unit.Services.Foundations.HttpClie
 
             return new StreamContent(stream);
         }
-        
-        private HttpResponseMessage CreateHttpResponseMessage(HttpStatusCode statusCode, string content)
+
+        private static Uri GetRandomUri(string relativeUrl)
         {
-            return new HttpResponseMessage(statusCode)
+            string randomHost = GetRandomString();
+
+            return new Uri($"https://{randomHost}/{relativeUrl}");
+        }
+
+        private static dynamic CreateRandomProperties()
+        {
+            string randomContent = GetRandomString();
+            string randomRelativeURL = GetRandomString();
+
+            return new
             {
-                Content = CreateStreamContent(content)
+                Url = GetRandomUri(randomRelativeURL),
+                RelativeUrl = randomRelativeURL,
+                HttpMethod = HttpMethod.Get,
+                ResponseStreamContent = CreateStreamContent(randomContent),
+                ResponseHttpStatus = HttpStatusCode.OK
             };
         }
-        
+
+        private static HttpRequestMessage CreateHttpRequestMessage(dynamic randomProperties) =>
+            new HttpRequestMessage()
+            {
+                RequestUri = randomProperties.Url,
+                Method = randomProperties.HttpMethod
+            };
+
+        private static HttpResponseMessage CreateHttpResponseMessage(dynamic randomProperties)
+        {
+            return new HttpResponseMessage(randomProperties.ResponseHttpStatus)
+            {
+                Content = randomProperties.ResponseStreamContent
+            };
+        }
+
+        private static Models.HttpClients.HttpClient CreateHttpClient(dynamic randomProperties)
+        {
+            return new Models.HttpClients.HttpClient
+            {
+                HttpRequest = new HttpRequest
+                {
+                    RelativeUrl = randomProperties.RelativeUrl
+                }
+            };
+        }
+
+        private static Models.HttpClients.HttpClient CreateHttpClientResponse(Models.HttpClients.HttpClient httpClient, dynamic randomProperties)
+        {
+            Models.HttpClients.HttpClient clonedHttpClient = httpClient.DeepClone();
+
+            clonedHttpClient.HttpResponse = new HttpResponse
+            {
+                StreamContent = randomProperties.ResponseStreamContent
+            };
+
+            return clonedHttpClient;
+        }
+
     }
 }
