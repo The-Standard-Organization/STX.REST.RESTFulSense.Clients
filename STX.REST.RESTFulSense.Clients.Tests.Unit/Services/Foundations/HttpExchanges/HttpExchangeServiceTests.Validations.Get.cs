805074029131c0ede75c95817c00a0caaa01d70b
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2024-04-15 21:42:10 +0100

    ShouldThrowHttpExchangeValidationExceptionOnGetIfHttpExchangeIsNullAsync -> FAIL

---
+++ STX.REST.RESTFulSense.Clients.Tests.Unit/Services/Foundations/HttpExchanges/HttpExchangeServiceTests.Validations.Get.cs
@@ -0,0 +1,45 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using STX.REST.RESTFulSense.Clients.Models.Services.HttpExchanges;
+using STX.REST.RESTFulSense.Clients.Models.Services.HttpExchanges.Exceptions;
+using Xunit;
+
+namespace STX.REST.RESTFulSense.Clients.Tests.Unit.Services.Foundations.HttpExchanges
+{
+    public partial class HttpExchangeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowHttpExchangeValidationExceptionOnGetIfHttpExchangeIsNullAsync()
+        {
+            // given
+            HttpExchange nullHttpExchange = null;
+
+            var nullHttpExchangeException =
+                new NullHttpExchangeException(
+                    message: "Null HttpExchange error occurred, please fix to continue.");
+
+            var expectedHttpExchangeValidationException =
+                new HttpExchangeValidationException(
+                    message: "Http exchange validation errors occurred, please try again.",
+                    innerException: nullHttpExchangeException);
+
+            // when
+            ValueTask<HttpExchange> getAsyncTask =
+                httpExchangeService.GetAsync(nullHttpExchange);
+
+            HttpExchangeValidationException actualHttpExchangeValidationException =
+                await Assert.ThrowsAsync<HttpExchangeValidationException>(
+                    () => getAsyncTask.AsTask());
+            
+            //then
+            actualHttpExchangeValidationException.Should().BeEquivalentTo(
+                expectedHttpExchangeValidationException);
+            
+            this.httpBroker.VerifyNoOtherCalls();
+        }
+    }
+}
