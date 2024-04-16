555f0c19eb89e2a011d850daabf2248220ad2631
Raúl LB <rlorenb@gmail.com>
2024-04-16 19:36:59 +0200

    DATA: Adds proporsal for HttpExchangeRequest #35

---
+++ STX.REST.RESTFulSense.Clients/Models/Services/HttpExchanges/HttpExchangeRequest.cs
@@ -0,0 +1,16 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace STX.REST.RESTFulSense.Clients.Models.Services.HttpExchanges
+{
+    internal class HttpExchangeRequest
+    {
+        public string BaseAddress { get; init; }
+        public string RelativeUrl { get; init; }
+        public string HttpMethod { get; init; }
+        public string Version { get; init; }
+        public HttpExchangeHeaders Headers { get; init; }
+        public HttpExchangeContent Content { get; init; }
+    }
+}
