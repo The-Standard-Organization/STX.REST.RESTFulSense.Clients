657bffe0a2af0442e5a89eaa4c000fe9efaee0de
Raúl LB <rlorenb@gmail.com>
2024-04-16 22:23:25 +0200

    DATA: Add specific headers class and add headers properties

--- STX.REST.RESTFulSense.Clients/Models/Services/HttpExchanges/Headers/RangeHeader.cs
+++ STX.REST.RESTFulSense.Clients/Models/Services/HttpExchanges/Headers/RangeHeader.cs
@@ -2,13 +2,11 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System.IO;
-
-namespace STX.REST.RESTFulSense.Clients.Models.Services.HttpExchanges
+namespace STX.REST.RESTFulSense.Clients.Models.Services.HttpExchanges.Headers
 {
-    internal class HttpExchangeContent
+    internal class RangeHeader
     {
-        public HttpExchangeContentHeaders Headers { get; init; }
-        public Stream StreamContent { get; set; }
+        public string Unit { get; init; }
+        public RangeItemHeader[] Ranges { get; init; }
     }
 }
